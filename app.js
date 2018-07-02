d11709ac6c877097c8a9fe1a9fa731f7846229ae
Felipe Acquati <felipe_acq@yahoo.com.br>
2018-07-01 22:59:54 -0300

    2 controllers added: CurrencyValue & likeCtrl

--- app.js
+++ app.js
@@ -19,7 +19,31 @@ angular.module('myApp', [])
 
     return totalStringValue;
   }
-  
+})
+.controller('CurrencyValue', function($scope, $filter) {
+  $scope.value = 0;
+  $scope.currency = 0;
+
+  $scope.numToMoney = function() {
+    $scope.currency = $filter('currency')($scope.value, "USD$", 2);
+  }
+})
+.controller('LikeCtrl', function($scope) {
+  $scope.choice = "up";
+
+  $scope.message = function() {
+    var answer;
+    answer = $scope.choice === "up" ? "like" : "dislike"
+    return "You choose: " + answer + "  ";
+  }
+
+  $scope.like = function() {
+    $scope.choice = "up";
+  };
+
+  $scope.dislike = function() {
+    $scope.choice = "down";
+  };
 });
 
 })();
